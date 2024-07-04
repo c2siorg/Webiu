3b85f2372f0b6e6999286215c2448ef7c6ccf964
GillaArun <gillaarun7@gmail.com>
2024-07-04 22:36:17 +0530

    #30

--- src/app/page/community/community.component.ts
+++ src/app/page/community/community.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { Media, socialMedia } from '../../common/data/media';
+import { Contributor, contributors } from '../../common/data/contributer';
 
 @Component({
   selector: 'app-community',
   standalone: true,
   imports: [NavbarComponent],
   templateUrl: './community.component.html',
-  styleUrl: './community.component.scss',
+  styleUrls: ['./community.component.scss'],
 })
-export class CommunityComponent {}
+export class CommunityComponent {
+  icons: Media[] = socialMedia;
+  users: Contributor[] = this.shuffleArray(contributors);
+  private shuffleArray(array: any[]): any[] {
+    let currentIndex = array.length, randomIndex;
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+  }
+}
