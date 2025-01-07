b7f67aa9805a14cb5e2bfc8c0b5f33df1fb8e5f8
abhishek farshwal <abhishekfarshwal2634@gmail.com>
2025-01-07 23:49:12 +0530

    read contents from external file for gsoc page

---
+++ webiu-ui/src/app/common/data/gsoc.ts
@@ -0,0 +1,20 @@
+export const gsocData = {
+    "heroTitle": "C2SI GSOC 2024",
+    "heroDescription": "Welcome to the C2SI Google Summer of Code (GSoC) 2024 project ideas page.",
+    "details": "This is the 7th time that we are participating in the GSoC, we will use this page to develop possible project ideas that have on the above mentioned areas. Please note that anyone who is interested can participate in this process. You do not have to be a GSoC student or mentor to suggest possible project ideas.",
+    "guidelines": [
+        "There is a fixed time period for implementing and coding your ideas",
+        "Come up with attainable goals and you will be able to complete what you set out to do. You can always contact our mentors and community and get an idea about the workload and whether you might be able to complete them.",
+        "You are free to come up with your own ideas. The ideas should be about Internet of Things (IOT), Computer Security and Software Tools. Also if you love to work on any of these subjects but do not have an idea you can always contact us.",
+        "Lets Talk! The best way to solve problems that you might have is to contact our mentors and also our community.",
+        "We encourage you to do documentation so that we can keep track of your progress and also help you if things are not going according to plan. Although not compulsory we have a strong belief that this method can cut down your time to code and also the workload of the mentors drastically.",
+        "Please send your new project ideas to gsoc[at]c2si.com"
+    ],
+    "projects": [
+        "ProjectExplainer - Add knowledge graph based retrieval and explanation APIs for code and text modalities in documentation.",
+        "WebiU 2.0 :- c2siorg/SCoRe Lab website",
+        "Rust Cloud",
+        "Bug Connector",
+        "GDB UI"
+    ]
+};
--- webiu-ui/src/app/page/gsoc/gsoc.component.ts
+++ webiu-ui/src/app/page/gsoc/gsoc.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { gsocData } from '../../common/data/gsoc';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-gsoc',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,CommonModule],
   templateUrl: './gsoc.component.html',
   styleUrl: './gsoc.component.scss',
 })
-export class GsocComponent {}
+export class GsocComponent {
+  gsocData = gsocData;
+}
