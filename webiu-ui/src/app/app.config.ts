02df04de68874c73ffcdc3fa030e5aca7dcc7a16
abhishek farshwal <abhishekfarshwal2634@gmail.com>
2025-01-31 19:52:14 +0530

    removing redundant code

--- webiu-ui/src/app/app.config.ts
+++ webiu-ui/src/app/app.config.ts
@@ -1,8 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()],  
+  providers: [provideRouter(routes)]
 };
