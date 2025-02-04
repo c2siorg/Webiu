c06bc5972fc8634e651fb12468fb99b215b56310
Alisha <mahmoodalisha35@gmail.com>
2025-02-05 03:24:12 +0530

    feat: added search functionality to search by project name

--- webiu-ui/src/app/page/projects/projects.component.ts
+++ webiu-ui/src/app/page/projects/projects.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { ProjectsCardComponent } from '../../components/projects-card/projects-card.component';
 import { projectsData } from './projects-data';
 import { Project, ProjectResponse } from './project.model';
 import { environment } from '../../../environments/environment'; 
+
 @Component({
   selector: 'app-projects',
   standalone: true,
   imports: [
     CommonModule,
     HttpClientModule,
+    FormsModule,
     NavbarComponent,
     ProjectsCardComponent,
   ],
@@ -20,7 +23,9 @@ import { environment } from '../../../environments/environment';
 })
 export class ProjectsComponent implements OnInit {
   projectsData: Project[] = [];
+  filteredProjects: Project[] = [];
   isLoading = true;
+  searchQuery: string = '';
 
   constructor(private http: HttpClient) {}
 
@@ -34,16 +39,29 @@ export class ProjectsComponent implements OnInit {
       .subscribe({
         next: (response) => {
           this.projectsData = response.repositories;
+          this.filteredProjects = response.repositories; 
           this.isLoading = false;
         },
         error: (error) => {
           this.projectsData = projectsData.repositories;
+          this.filteredProjects = projectsData.repositories; 
           this.isLoading = false;
         },
         complete: () => {
           console.log('Fetch projects request completed.');
         },
       });
+  }
+
+  
+  filterProjectsByName(): void {
+    if (this.searchQuery) {
+      this.filteredProjects = this.projectsData.filter((project) =>
+        project.name.toLowerCase().includes(this.searchQuery.toLowerCase()) 
+      );
+    } else {
+      this.filteredProjects = this.projectsData; 
+    }
   }
 
   trackByProjectName(index: number, project: Project): string {
