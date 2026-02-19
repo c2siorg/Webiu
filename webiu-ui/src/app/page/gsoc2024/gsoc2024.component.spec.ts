de95c21aeb24c2ba9ceb16f5209865c34eed366b
Tarunya K <tarunya.programmer@gmail.com>
2026-02-20 04:26:07 +0530

    fix: AOT build template errors in gsoc2024 and expanded css budget

--- webiu-ui/src/app/page/gsoc2024/gsoc2024.component.spec.ts
+++ webiu-ui/src/app/page/gsoc2024/gsoc2024.component.spec.ts
@@ -1,18 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { GsocComponent } from './gsoc.component';
+import { Gsoc2024Component } from './gsoc2024.component';
 
-describe('GsocComponent', () => {
-  let component: GsocComponent;
-  let fixture: ComponentFixture<GsocComponent>;
+describe('Gsoc2024Component', () => {
+  let component: Gsoc2024Component;
+  let fixture: ComponentFixture<Gsoc2024Component>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GsocComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(GsocComponent);
+      imports: [Gsoc2024Component],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Gsoc2024Component);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
