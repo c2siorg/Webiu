22545912f08453d5c53349dbf4a178a6e9ba37cd
Om Sharma <om.debug@gmail.com>
2026-02-20 04:10:18 +0530

    lint error fix-2

--- webiu-ui/src/app/page/homepage/homepage.component.spec.ts
+++ webiu-ui/src/app/page/homepage/homepage.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 import { HomepageComponent } from './homepage.component';
 
@@ -8,10 +10,22 @@ describe('HomepageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomepageComponent]
-    })
-    .compileComponents();
-    
+      imports: [HomepageComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: () => null
+              }
+            },
+            params: of({})
+          }
+        }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(HomepageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
