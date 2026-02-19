dbf3c7d23310dd79d7d0944ccbc0fc2d872af8d0
Tarunya K <tarunya.programmer@gmail.com>
2026-02-20 04:51:38 +0530

    feat: Add structured responsive footer with dark mode support (#232)

---
+++ webiu-ui/src/app/components/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+})
+export class FooterComponent {
+  currentYear = new Date().getFullYear();
+}
