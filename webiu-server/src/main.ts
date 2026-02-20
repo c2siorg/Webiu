2e7474d24e19d26b6d18bf3178e9a292f5116b01
sandyp025 <sandipmandal02.sm@gmail.com>
2026-02-20 17:04:19 +0530

    feat: add Helmet middleware to set HTTP security headers

--- webiu-server/src/main.ts
+++ webiu-server/src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.use(helmet());
 
   const frontendUrl = configService.get<string>(
     'FRONTEND_BASE_URL',
