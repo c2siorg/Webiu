c92896e3173121d22a28ecd4de12663eef2f62da
sandyp025 <sandipmandal02.sm@gmail.com>
2026-02-20 17:25:55 +0530

    fix: replace N+1 contributor API calls with single batch endpoint

--- webiu-server/src/user/user.controller.spec.ts
+++ webiu-server/src/user/user.controller.spec.ts
@@ -1,14 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { GithubService } from '../github/github.service';
 
 describe('UserController', () => {
   let controller: UserController;
 
+  const mockGithubService = {
+    getPublicUserProfile: jest.fn(),
+    getUserFollowersAndFollowing: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService],
+      providers: [
+        UserService,
+        { provide: GithubService, useValue: mockGithubService },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
