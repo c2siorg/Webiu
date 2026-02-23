b0dcf9e009fda077b0816dc0eb2e04db5c06c60f
ADITYA-CODE-SOURCE <adityavishe67@example.com>
2026-02-23 09:26:17 +0530

    fix: Revert navbar test to original

--- webiu-ui/src/app/components/navbar/navbar.component.spec.ts
+++ webiu-ui/src/app/components/navbar/navbar.component.spec.ts
@@ -1,26 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { NavbarComponent } from './navbar.component';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
-  let httpMock: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavbarComponent, RouterTestingModule, HttpClientTestingModule],
+      imports: [NavbarComponent, RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
-    httpMock = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
-  });
-
-  afterEach(() => {
-    httpMock.verify();
   });
 
   it('should create', () => {
