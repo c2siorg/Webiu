56d2a98225c4ea4159be96e9df613f9d36024ff6
ADITYA-CODE-SOURCE <adityavishe67@example.com>
2026-02-23 09:23:09 +0530

    fix: Remove unused import in navbar test

--- webiu-ui/src/app/components/navbar/navbar.component.spec.ts
+++ webiu-ui/src/app/components/navbar/navbar.component.spec.ts
@@ -2,7 +2,6 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { NavbarComponent } from './navbar.component';
-import { environment } from '../../../environments/environment';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
