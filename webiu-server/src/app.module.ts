e16ab42faa8bb32341aa5842e4d879e1b73363d6
utkarsh_raj_13 <49344502+rajutkarsh07@users.noreply.github.com>
2026-02-24 22:35:23 +0530

    Update app.module.ts

--- webiu-server/src/app.module.ts
+++ webiu-server/src/app.module.ts
@@ -5,7 +5,8 @@ import { ThrottlerModule } from '@nestjs/throttler';
 import { GqlThrottlerGuard } from './graphql/gql-throttler.guard';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import depthLimit from 'graphql-depth-limit';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const depthLimit = require('graphql-depth-limit');
 import { Request, Response } from 'express';
 import { AppController } from './app.controller';
 import { CommonModule } from './common/common.module';
