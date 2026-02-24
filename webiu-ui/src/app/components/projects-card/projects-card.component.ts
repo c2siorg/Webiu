23af720176c1cdd49e04fa970c3e9640d9ce1378
Ahmed Elgyar <elgyarahmed71@gmail.com>
2026-02-24 03:29:54 +0200

    fix(ui): use input data for issues and PR counts in projects card

--- webiu-ui/src/app/components/projects-card/projects-card.component.ts
+++ webiu-ui/src/app/components/projects-card/projects-card.component.ts
@@ -1,18 +1,15 @@
-import { Component, Input, OnInit, inject } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
-import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { environment } from '../../../environments/environment';
-
 
 @Component({
   selector: 'app-projects-card',
   standalone: true,
-  imports: [HttpClientModule, CommonModule],
+  imports: [CommonModule],
   templateUrl: './projects-card.component.html',
   styleUrls: ['./projects-card.component.scss'],
 })
-export class ProjectsCardComponent implements OnInit {
+export class ProjectsCardComponent {
   @Input() name!: string;
   @Input() description: string | null = '';
   @Input() issue!: number;
@@ -24,33 +21,6 @@ export class ProjectsCardComponent implements OnInit {
   @Input() updatedAt!: string;
   @Input() org!: string;
   @Input() repo!: string;
-
-  issueCount = 0;
-  pullRequestCount = 0;
-  initialized = false;
-
-  private http = inject(HttpClient);
-
-
-  ngOnInit(): void {
-    if (!this.initialized) {
-      this.fetchIssuesAndPRs();
-    }
-  }
-
-  fetchIssuesAndPRs(): void {
-    const apiUrl = `${environment.serverUrl}/api/issues/issuesAndPr?org=${this.org}&repo=${this.repo}`;
-    this.http.get<{ issues: number; pullRequests: number }>(apiUrl).subscribe(
-      (data) => {
-        this.issueCount = data.issues;
-        this.pullRequestCount = data.pullRequests;
-        this.initialized = true;
-      },
-      (error) => {
-        console.error('Failed to fetch issues and PRs:', error);
-      }
-    );
-  }
 
   public detailsVisible = false;
 
