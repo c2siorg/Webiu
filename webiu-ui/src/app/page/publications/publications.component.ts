5bff913d6f58d84325e7be043a387ed1dbb3dda0
StevenStn <melahelstone@gmail.com>
2026-03-19 01:29:11 +0100

    fix: remove duplicate entries on publications page

--- webiu-ui/src/app/page/publications/publications.component.ts
+++ webiu-ui/src/app/page/publications/publications.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { PublicationsCardComponent } from '../../components/publications-card/publications-card.component';
 import { publicationsData } from './publications-data';
@@ -12,5 +11,16 @@ import { publicationsData } from './publications-data';
   styleUrls: ['./publications.component.scss'],
 })
 export class PublicationsComponent {
-  publicationsData = publicationsData;
+  publicationsData = this.deduplicatePublications(publicationsData);
+
+  private deduplicatePublications(
+    data: { heading: string; link: string; issued_by: string; description: string }[]
+  ) {
+    const seen = new Set<string>();
+    return data.filter((pub) => {
+      if (seen.has(pub.link)) return false;
+      seen.add(pub.link);
+      return true;
+    });
+  }
 }
